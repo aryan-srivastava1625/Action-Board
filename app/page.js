c3edd9737bb3c232a22bca3b61114c47927e4e93
aryan-srivastava1625 <ap9140@srmist.edu.in>
2024-12-05 14:22:32 +0530

    making todo list

--- app/page.js
+++ app/page.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Aryan's Todo List</h1>
+    <form>
+      <input type='text' className='text-2xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Make Your List'/>
+      <input type='text' className='text-2xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Enter Description Here'/>
+      <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5'>Tasks</button>
+    </form>
+
+    </>
   )
 }
 
